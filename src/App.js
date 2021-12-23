d8e0ebbbe014cd7ce83518cc08e3b87a5adc196d
fan1140427762 <1140427762@qq.com>
2021-12-23 17:20:27 +0800

    测试提交

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 
 import React, { Component } from 'react'
-import Hello from './test1/Hello';
+import Hello from './components/test1/Hello';
+import Welcome from './components/welcome/Welcome';
 export default class App extends Component {
   render() {
     return (
       <div>
         <Hello />
+        <Welcome />
       </div>
     )
   }
